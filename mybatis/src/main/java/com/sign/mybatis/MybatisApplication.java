75bfd01902a6d2984e814e11f548ffcf9dc6649f
GDKJAdmin <975194072@qq.com>
2024-10-17 09:30:45 +0800

    测试提交代码

--- mybatis/src/main/java/com/sign/mybatis/MybatisApplication.java
+++ mybatis/src/main/java/com/sign/mybatis/MybatisApplication.java
@@ -1,13 +1,28 @@
 package com.sign.mybatis;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class MybatisApplication {
+    @Autowired
+    private String words = "cebtech"
 
     public static void main(String[] args) {
         SpringApplication.run(MybatisApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception{
+        hello(this.words);
+    }
+
+    public void hello(String words){
+        log.info("Hello " + words + " !!!")
     }
 
 }
